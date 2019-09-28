208422c156c5eb6a52471e32806f1d33eba0978f
Luca Benetti <55714888+lucabenetti@users.noreply.github.com>
2019-09-28 16:13:05 -0300

    Create selectionsortRecursive.c

---
+++ selectionsortRecursive.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void selectionSort(int *vetor, int i, int n) {
+	int j;
+	int aux;
+	int min = i;
+
+	for (j = i + 1; j < n; j++) {
+		if (vetor[j] < vetor[min])	min = j;
+	}
+
+	aux = vetor[min];
+	vetor[min] = vetor[i];
+	vetor[i] = aux;
+
+	if (i + 1 < n) {
+		selectionSort(vetor, i + 1, n);
+	}
+}
+
+int main() {
+	int n;
+	int i;
+
+	scanf("%d", &n);
+
+	int *vetor = (int *) malloc (n * sizeof(int));	
+
+	for(i = 0; i < n; i++) {
+		scanf("%d", &vetor[i]);	
+	}
+ 
+	selectionSort(vetor, 0, n);
+
+	for(i = 0; i < n; i++) {
+		printf("%d ", vetor[i]);	
+	} printf("\n");
+	
+	free(vetor);
+	return 0;
+}
