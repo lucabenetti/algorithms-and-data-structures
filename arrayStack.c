9215c65f5d4ba8a27c809952b6712c121e6c6045
Luca Benetti <55714888+lucabenetti@users.noreply.github.com>
2019-11-30 15:33:22 -0300

    Update arrayStack.c

--- arrayStack.c
+++ arrayStack.c
@@ -9,30 +9,27 @@ Pilha * pilha_copia (Pilha * p);
 void pilha_libera (Pilha * p);
 int pilha_insere (Pilha * p, char * elemento); // push
 char * pilha_remove (Pilha * p); // pop
-char * pilha_obtem_topo  (Pilha * p); // top
-int pilha_obtem_tamanho  (Pilha * p);
+char * pilha_obtem_topo (Pilha * p); // top
+int pilha_obtem_tamanho (Pilha * p);
 int pilha_se_vazia (Pilha * p);
 int pilha_se_cheia (Pilha * p);
-char * pilha_imprime  (Pilha * p);
+char * pilha_imprime (Pilha * p);
  
 struct pilha {
     int tamanho;
     int maxTamanho;
-    char **elementos;
+    char ** elementos;
 };
  
-Pilha *pilha_cria(int maxTamanho) {
-    Pilha *q = malloc (sizeof(Pilha));
-   
-    if(q != NULL) {
-        q->elementos = malloc (sizeof(char *) * maxTamanho);
+Pilha * pilha_cria (int maxTamanho) {
+    Pilha * q = (Pilha *) malloc(sizeof(Pilha));
+    if (q!=NULL) {
         q->tamanho = 0;
         q->maxTamanho = maxTamanho;
+        q->elementos = (char **) malloc(sizeof(char *) * maxTamanho);
     }
- 
     return q;
 }
- 
 Pilha * pilha_copia (Pilha * p) {
     Pilha *q = pilha_cria(p->maxTamanho);
    
@@ -47,82 +44,50 @@ Pilha * pilha_copia (Pilha * p) {
    
     return q;
 }
- 
 void pilha_libera (Pilha * p) {
-    int i;
-   
-    for(i = 0; i < p->tamanho; i++) {
-        free(p->elementos[i]);
-    }
-   
+    free(p->elementos);
     free(p);
 }
- 
 int pilha_insere (Pilha * p, char * elemento) {
-    if(pilha_se_cheia(p) || p == NULL) return 0;
-       
-    p->elementos[p->tamanho] = malloc (strlen(elemento)+1);
- 
-    strcpy(p->elementos[p->tamanho], elemento);
- 
-    p->tamanho++;  
-   
-    return 1;
+    if(p==NULL)return 0;
+    if(pilha_se_cheia(p))return 0;
+    p->elementos[p->tamanho]=(char *) malloc((strlen(elemento)+1)*sizeof(char));
+    strcpy(p->elementos[p->tamanho],elemento);
+    p->tamanho++;
+    return 1; // insercao com sucesso    
 }
- 
 char * pilha_remove (Pilha * p) {
-    char *aux = malloc (strlen(p->elementos[p->tamanho-1])+1);
-	
-    strcpy(aux, p->elementos[p->tamanho-1]);
-   
-    p->elementos[p->tamanho-1] = NULL;
-   
+    
+    if(p==NULL || p->tamanho==0) return 0;
+    char *temp = (char*)malloc((strlen(p->elementos[p->tamanho-1])+1)*sizeof(char));
+    strcpy(temp,p->elementos[p->tamanho-1]);
     p->tamanho--;
-   
-    return aux;
+    return temp;
 }
- 
 char * pilha_obtem_topo  (Pilha * p) {
-    if(p == NULL || p->tamanho == 0) return '\0';
+    if(p==NULL || p->tamanho==0)return "\0";
     return p->elementos[p->tamanho-1];
 }
 int pilha_obtem_tamanho  (Pilha * p) {
-    if (p == NULL) return -1;
-   
+    if(p==NULL) return -1;
     return p->tamanho;
 }
 int pilha_se_vazia (Pilha * p) {
-    if(p == NULL) return -1;
-   
-    if(p->tamanho == 0) return 1;
+    if(p==NULL)return -1;
+    return (p->tamanho==0);
 }
 int pilha_se_cheia (Pilha * p) {
-    if(p == NULL) return -1;
-   
-    else if(p->tamanho == p->maxTamanho) return 1;
- 
-    else return 0;
+    if(p==NULL)return -1;
+    return (p->tamanho==p->maxTamanho);    
 }
 char * pilha_imprime  (Pilha * p) {
-    char *aux = malloc (10000);
-    aux[0] = '\0';
- 
-    if(p->tamanho == 0 || p == NULL) return aux;
- 
+    char * aux = (char *) malloc(sizeof(char)*10*p->maxTamanho);
+    aux [0] = '\0';
     int i;
- 
-    for(i = 0; i < p->tamanho; i++) {
-        if(i == 0) sprintf(aux, "%s ", p->elementos[i]);
-        else sprintf(aux, "%s%s ", aux, p->elementos[i]);
-    }              
-                   
-                   
+    for (i=p->tamanho-1; i>=0; i--) {
+        strcat(aux, p->elementos[i]);
+        strcat(aux, " ");
+    }
+    aux = realloc(aux, strlen(aux)+1);
     return aux;
 }
- 
- 
-int main () {
-	return 0;
-}
-
-
